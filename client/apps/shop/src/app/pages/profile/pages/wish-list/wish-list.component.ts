d94d7f60be68483edc549760c533787801203c9b
Kole <h.kolaric92@gmail.com>
2019-04-25 16:07:32 +0200

    fix(customer-list module): loader implementation #54

--- client/apps/shop/src/app/pages/profile/pages/wish-list/wish-list.component.ts
+++ client/apps/shop/src/app/pages/profile/pages/wish-list/wish-list.component.ts
@@ -2,8 +2,8 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {STATIC_CONFIG} from '@jf/consts/static-config.const';
 import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
-import {forkJoin, Observable, of} from 'rxjs';
-import {map, switchMap} from 'rxjs/operators';
+import {BehaviorSubject, forkJoin, Observable, of} from 'rxjs';
+import {map, switchMap, tap} from 'rxjs/operators';
 import {Product} from '../../../../shared/interfaces/product.interface';
 import {StateService} from '../../../../shared/services/state/state.service';
 
@@ -17,10 +17,12 @@ export class WishListComponent implements OnInit {
   constructor(private state: StateService, private afs: AngularFirestore) {}
 
   wishList$: Observable<Product[]>;
+  loading$ = new BehaviorSubject(true);
 
   ngOnInit() {
     this.wishList$ = this.state.user$.pipe(
       switchMap(user => {
+        this.loading$.next(false);
         /**
          * If the user has items in his wish list
          * we populate them with products from
@@ -44,6 +46,9 @@ export class WishListComponent implements OnInit {
         } else {
           return of([]);
         }
+      }),
+      tap(() => {
+        this.loading$.next(true);
       })
     );
   }
