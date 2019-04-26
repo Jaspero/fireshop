77e365afc276895e304ca619aa259123da650432
Kole <h.kolaric92@gmail.com>
2019-04-26 15:12:04 +0200

    fix(my-profile module): profile image upload #3

--- client/apps/shop/src/app/pages/profile/profile.component.ts
+++ client/apps/shop/src/app/pages/profile/profile.component.ts
@@ -1,6 +1,17 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {Router} from '@angular/router';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {forkJoin, from} from 'rxjs';
+import {readFile} from '@jf/utils/read-file';
+import {map, switchMap, take} from 'rxjs/operators';
 
 @Component({
   selector: 'jfs-profile',
@@ -9,7 +20,15 @@ import {Router} from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent implements OnInit {
-  constructor(public afAuth: AngularFireAuth, private router: Router) {}
+  constructor(
+    public afAuth: AngularFireAuth,
+    private router: Router,
+    private afs: AngularFireStorage,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  @ViewChild('file')
+  fileEl: ElementRef<HTMLInputElement>;
 
   links = [
     {
@@ -31,9 +50,29 @@ export class ProfileComponent implements OnInit {
   ];
 
   activeLink: any;
+  downloadURL: any;
 
   ngOnInit() {
     const url = this.router.url.replace('/my-profile/', '');
     this.activeLink = this.links.find(val => val.route === url);
+  }
+
+  openFileUpload() {
+    this.fileEl.nativeElement.click();
+  }
+
+  filesImage(file) {
+    const fileToUpload = Array.from(file)[0];
+    from(
+      this.afs.upload('my-profile-pic', fileToUpload, {
+        contentType: fileToUpload.type
+      })
+    )
+      .pipe(switchMap(res => res.ref.getDownloadURL()))
+      .subscribe(res => {
+        console.log('res', res);
+        this.downloadURL = res;
+        this.cdr.detectChanges();
+      });
   }
 }
