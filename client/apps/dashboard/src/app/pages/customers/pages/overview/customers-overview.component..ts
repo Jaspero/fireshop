09f58871946825640959fe4e80edbf938f175671
svendjanis <sven.djanis@gmail.com>
2019-05-02 13:59:29 +0200

    chore(customers): working on customers #48

--- client/apps/dashboard/src/app/pages/customers/pages/overview/customers-overview.component..ts
+++ client/apps/dashboard/src/app/pages/customers/pages/overview/customers-overview.component..ts
@@ -1,12 +1,29 @@
 import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {ActivatedRoute} from '@angular/router';
+import {FirebaseOperator} from '@jf/enums/firebase-operator.enum';
+import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
 
 @Component({
-  selector: 'jfs-overview',
+  selector: 'jfsc-overview',
   templateUrl: './customers-overview.component.html',
   styleUrls: ['./customers-overview.component.css']
 })
 export class CustomersOverviewComponent implements OnInit {
-  constructor() {}
+  constructor(public acr: ActivatedRoute, private afs: AngularFirestore) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.afs
+      .collection(FirestoreCollections.Reviews, ref => {
+        return ref.where(
+          'customerId',
+          FirebaseOperator.Equal,
+          this.acr.params.value.id
+        );
+      })
+      .get()
+      .subscribe(val => {
+        console.log(val);
+      });
+  }
 }
