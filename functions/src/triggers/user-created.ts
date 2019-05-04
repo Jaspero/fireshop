2e3e42bcd49eba4d9ab372fb14a653eb314f998e
Filip Lauc <filip.lauc93@gmail.com>
2019-05-04 14:14:45 +0200

    feat(functions): adding user to mailchimp list on signup :zap: #63

--- functions/src/triggers/user-created.ts
+++ functions/src/triggers/user-created.ts
@@ -1,5 +1,7 @@
 import {auth, firestore} from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import * as rp from 'request-promise-native';
+import {ENV_CONFIG} from '../consts/env-config.const';
 import {parseEmail} from '../utils/parse-email';
 
 export const userCreated = functions.auth.user().onCreate(async user => {
@@ -17,6 +19,26 @@ export const userCreated = functions.auth.user().onCreate(async user => {
     await auth().setCustomUserClaims(user.uid, customClaims);
   } else {
     await parseEmail(user.email, 'Welcome to Fireshop', 'user-created', user);
+  }
+
+  try {
+    await rp({
+      method: 'POST',
+      uri: `https://us20.api.mailchimp.com/3.0/lists/${
+        ENV_CONFIG.mailchimp.list
+      }/members/`,
+      auth: {
+        user: 'username',
+        pass: ENV_CONFIG.mailchimp.token
+      },
+      body: {
+        email_address: user.email,
+        status: 'subscribed'
+      },
+      json: true
+    });
+  } catch (e) {
+    console.error(e);
   }
 
   return true;
