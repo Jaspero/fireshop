ee9945c34ad73e282d9bee2fcc29a2793419fe4c
Filip Lauc <filip.lauc93@gmail.com>
2019-05-04 23:26:59 +0200

    feat(functions): export-data :construction: #7

--- functions/src/rest/export-data.ts
+++ functions/src/rest/export-data.ts
@@ -1,5 +1,35 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import {HttpStatus} from '../enums/http-status.enum';
 
 export const exportData = functions.https.onRequest((req, res) => {
-  res.json({});
+  async function exec() {
+    const {collection, type, ids} = req.body;
+
+    const docs = (await admin
+      .firestore()
+      .collection(collection)
+      .get()).docs.reduce((acc, doc) => {
+      if (!ids || !ids.includes(doc.id)) {
+        acc.push({
+          ...doc.data(),
+          id: doc.id
+        });
+      }
+
+      return acc;
+    }, []);
+  }
+
+  exec()
+    .then(({type, data}) => {
+      res.setHeader('Content-type', `application/${type}`);
+      return res.send(data);
+    })
+    .catch(error => {
+      console.error(error);
+      res
+        .status(HttpStatus.InternalServerError)
+        .send({error: error.toString()});
+    });
 });
