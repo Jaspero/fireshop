c853b7b0e84b1f1841ea0664fcb0630c9e1311a6
Kole <h.kolaric92@gmail.com>
2019-05-08 16:29:36 +0200

    feat(import): busboy working :tada: #8

--- functions/src/rest/import-data.ts
+++ functions/src/rest/import-data.ts
@@ -2,16 +2,29 @@ import * as Busboy from 'busboy';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as functions from 'firebase-functions';
-import * as csv from 'csvtojson';
 
 const app = express();
 app.use(cors());
 
 app.post('/', (req, res) => {
   const busboy = new Busboy({headers: req.headers});
-  console.log('busboy', busboy);
 
-  res.json({response: busboy});
+  let fileData = '';
+
+  busboy.on('file', (fieldname, file) => {
+    file.on('data', data => {
+      fileData += data.toString();
+    });
+  });
+
+  busboy.on('finish', () => {
+    // TODO: start here
+    console.log(fileData);
+
+    res.json({fileData});
+  });
+
+  busboy.end(req['rawBody']);
 });
 
 export const importData = functions.https.onRequest(app);
