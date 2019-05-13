6890f4b8eff93b5847b9accfbee570b9fb674440
svendjanis <sven.djanis@gmail.com>
2019-05-13 15:43:57 +0200

    feat(products): working on products overview #133

---
+++ client/apps/dashboard/src/app/pages/products/pages/overview/products-overview.component.ts
@@ -0,0 +1,71 @@
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {ActivatedRoute} from '@angular/router';
+import {FirebaseOperator} from '@jf/enums/firebase-operator.enum';
+import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
+import {forkJoin, Observable} from 'rxjs';
+import {map, switchMap} from 'rxjs/operators';
+
+@Component({
+  selector: 'jfsc-products-overview',
+  templateUrl: './products-overview.component.html',
+  styleUrls: ['./products-overview.component.css']
+})
+export class ProductsOverviewComponent implements OnInit {
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    private afs: AngularFirestore
+  ) {}
+
+  data$: Observable<any>;
+
+  ngOnInit() {
+    this.data$ = this.activatedRoute.params.pipe(
+      switchMap(identifier => {
+        return forkJoin(
+          this.afs
+            .collection(FirestoreCollections.Orders, ref =>
+              ref.where('orderItems[id]', FirebaseOperator.Equal, identifier.id)
+            )
+            .get()
+            .pipe(
+              map(actions =>
+                actions.docs.map(action => ({
+                  id: action.id,
+                  ...action.data()
+                }))
+              )
+            ),
+
+          this.afs
+            .collection(FirestoreCollections.Reviews, ref =>
+              ref.where('productId', FirebaseOperator.Equal, identifier.id)
+            )
+            .get()
+            .pipe(
+              map(actions =>
+                actions.docs.map(action => ({
+                  id: action.id,
+                  ...action.data()
+                }))
+              )
+            ),
+
+          this.afs
+            .collection(FirestoreCollections.Customers, ref =>
+              ref.where('wishList', 'array-contains', identifier.id)
+            )
+            .get()
+            .pipe(
+              map(actions =>
+                actions.docs.map(action => ({
+                  id: action.id,
+                  ...action.data()
+                }))
+              )
+            )
+        );
+      })
+    );
+  }
+}
