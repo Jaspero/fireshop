f92a363555048202774ff3acd2221d39ce58ed1c
svendjanis <sven.djanis@gmail.com>
2019-05-14 15:24:05 +0200

    feat(dashboard): query for orders in the current month #14

--- client/apps/dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ client/apps/dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,11 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {FirebaseOperator} from '@jf/enums/firebase-operator.enum';
+import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
+import {Order} from '@jf/interfaces/order.interface';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {firstOfMonth} from '../../shared/utils/first-date-month';
 
 @Component({
   selector: 'jfsc-dashboard',
@@ -7,6 +14,43 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
-  ngOnInit() {}
+  constructor(private afs: AngularFirestore) {}
+
+  orders = [];
+  orders$: Observable<Order[]>;
+  ordersInMonth$: Observable<Order[]>;
+
+  ngOnInit() {
+    this.orders$ = this.afs
+      .collection<Order>(FirestoreCollections.Orders, ref => {
+        return ref.orderBy('createdOn', 'desc').limit(3);
+      })
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(action => ({
+            id: action.payload.doc.id,
+            ...action.payload.doc.data()
+          }))
+        )
+      );
+
+    this.ordersInMonth$ = this.afs
+      .collection<Order>(FirestoreCollections.Orders, ref => {
+        return ref.where(
+          'createdOn',
+          FirebaseOperator.LargerThenOrEqual,
+          firstOfMonth()
+        );
+      })
+      .snapshotChanges()
+      .pipe(
+        map(actions =>
+          actions.map(action => ({
+            id: action.payload.doc.id,
+            ...action.payload.doc.data()
+          }))
+        )
+      );
+  }
 }
