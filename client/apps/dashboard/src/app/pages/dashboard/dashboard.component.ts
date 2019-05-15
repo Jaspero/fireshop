3af5ce7f0db3cd45e56ff288a613d8271420193c
svendjanis <sven.djanis@gmail.com>
2019-05-15 08:51:09 +0200

    chore(dashboard): working on pulling one discount, product, category #14

--- client/apps/dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ client/apps/dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {ActivatedRoute} from '@angular/router';
 import {FirebaseOperator} from '@jf/enums/firebase-operator.enum';
 import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
+import {Category} from '@jf/interfaces/category.interface';
+import {Discount} from '@jf/interfaces/discount.interface';
 import {Order} from '@jf/interfaces/order.interface';
+import {Product} from '@jf/interfaces/product.interface';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {firstOfMonth} from '../../shared/utils/first-date-month';
@@ -14,11 +18,19 @@ import {firstOfMonth} from '../../shared/utils/first-date-month';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
-  constructor(private afs: AngularFirestore) {}
+  constructor(
+    private afs: AngularFirestore,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   orders = [];
   orders$: Observable<Order[]>;
   ordersInMonth$: Observable<Order[]>;
+  data$: Observable<{
+    category: Category[];
+    products: Product[];
+    discounts: Discount[];
+  }>;
 
   ngOnInit() {
     this.orders$ = this.afs
