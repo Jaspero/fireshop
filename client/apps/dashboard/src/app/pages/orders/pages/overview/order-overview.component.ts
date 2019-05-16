b42912a71b0c9cb924ef68039ee044f61713ba9d
svendjanis <sven.djanis@gmail.com>
2019-05-16 13:24:38 +0200

    feat(order): reviews in order-overview  #150

--- client/apps/dashboard/src/app/pages/orders/pages/overview/order-overview.component.ts
+++ client/apps/dashboard/src/app/pages/orders/pages/overview/order-overview.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {ActivatedRoute} from '@angular/router';
-import {STATIC_CONFIG} from '@jf/consts/static-config.const';
+import {FirebaseOperator} from '@jf/enums/firebase-operator.enum';
 import {FirestoreCollections} from '@jf/enums/firestore-collections.enum';
-import {forkJoin, Observable} from 'rxjs';
+import {Order} from '@jf/interfaces/order.interface';
+import {Review} from '@jf/interfaces/review.interface';
+import {forkJoin, Observable, of} from 'rxjs';
 import {map, switchMap} from 'rxjs/operators';
-import {log} from 'util';
 
 @Component({
   selector: 'jfsc-order-overview',
@@ -20,12 +21,12 @@ export class OrderOverviewComponent implements OnInit {
 
   data$: Observable<any>;
 
-  // this.afs.collection(`${FirestoreCollections.Products}-${STATIC_CONFIG.lang}`)
-
   ngOnInit() {
     this.data$ = this.activatedRoute.params.pipe(
-      switchMap(identifier =>
-        this.afs
+      switchMap(identifier => {
+        let order: Order;
+
+        return this.afs
           .collection(FirestoreCollections.Orders)
           .doc(identifier.id)
           .get()
@@ -35,7 +36,49 @@ export class OrderOverviewComponent implements OnInit {
               ...res.data()
             }))
           )
-      )
+          .pipe(
+            switchMap((data: Order) => {
+              order = data;
+
+              if (data.customerId) {
+                return forkJoin(
+                  data.orderItems.map(productId =>
+                    this.afs
+                      .collection(FirestoreCollections.Reviews, ref => {
+                        return ref
+                          .where(
+                            'customerId',
+                            FirebaseOperator.Equal,
+                            data.customerId
+                          )
+                          .where('orderId', FirebaseOperator.Equal, data.id)
+                          .where(
+                            'productId',
+                            FirebaseOperator.Equal,
+                            productId
+                          );
+                      })
+                      .get()
+                      .pipe(
+                        map(actions =>
+                          actions.docs.map(action => ({
+                            id: action.id,
+                            ...(action.data() as Review)
+                          }))
+                        )
+                      )
+                  )
+                );
+              } else {
+                return of([]);
+              }
+            }),
+            map(reviews => ({
+              order,
+              reviews
+            }))
+          );
+      })
     );
   }
 }
