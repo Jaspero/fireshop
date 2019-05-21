b628923e913af65ea5078a47e6e53cd2c62c3325
svendjanis <sven.djanis@gmail.com>
2019-05-21 13:33:43 +0200

    feat(slider): improve configuration #5

--- client/src/app/modules/module-instance/components/fields/slider/slider.component.ts
+++ client/src/app/modules/module-instance/components/fields/slider/slider.component.ts
@@ -1,5 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {pipe} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {FieldComponent, FieldData} from '../../field/field.component';
+
+interface SliderData extends FieldData {
+  thumbLabel: boolean;
+  tickInterval: number;
+  min: number;
+  max: number;
+}
 
 @Component({
   selector: 'jms-slider',
@@ -7,4 +16,8 @@ import {FieldComponent, FieldData} from '../../field/field.component';
   styleUrls: ['./slider.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SliderComponent extends FieldComponent<FieldData> {}
+export class SliderComponent extends FieldComponent<SliderData> {
+  ngOnInit() {
+    console.log(this.cData);
+  }
+}
