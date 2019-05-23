24cc03013a4fad72c4994a7deaf2f7214b530b95
svendjanis <sven.djanis@gmail.com>
2019-05-23 10:45:47 +0200

    fix(sale): updated interfaces and fixes in typo  #167

--- client/shared/interfaces/sales.interface.ts
+++ client/shared/interfaces/sales.interface.ts
@@ -2,4 +2,10 @@ export interface Sales {
   id: number;
   name: string;
   createdOn: number;
+  startingDate: Date;
+  endingDate: Date;
+  type: string;
+  active: boolean;
+  ribbonProduct: boolean;
+  limitedNumber: boolean;
 }
