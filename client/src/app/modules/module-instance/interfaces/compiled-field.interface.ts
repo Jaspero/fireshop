4eb650161549848f4e8d0b0837da8154af1fdc03
svendjanis <sven.djanis@gmail.com>
2019-06-14 10:32:48 +0200

    fix(app): adding to interfaces closes #43

--- client/src/app/modules/module-instance/interfaces/compiled-field.interface.ts
+++ client/src/app/modules/module-instance/interfaces/compiled-field.interface.ts
@@ -5,6 +5,7 @@ import {Control} from './control.type';
 export interface CompiledField {
   pointer: string;
   label: string;
+  placeholder: string;
   control: Control;
   portal: ComponentPortal<FieldComponent<any>>;
   validation: any;
