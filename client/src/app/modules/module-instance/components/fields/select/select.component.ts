a76456f3ace417465ccafdfb1cc533c0c12e94de
Filip Lauc <filip.lauc93@gmail.com>
2019-06-20 15:07:13 +0200

    feat(Select): allow for populate :zap: closes #59

--- client/src/app/modules/module-instance/components/fields/select/select.component.ts
+++ client/src/app/modules/module-instance/components/fields/select/select.component.ts
@@ -1,8 +1,29 @@
-import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Inject,
+  OnInit
+} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+import {FirebaseOperator} from '../../../../../shared/enums/firebase-operator.enum';
+import {COMPONENT_DATA} from '../../../utils/create-component-injector';
 import {FieldComponent, FieldData} from '../../field/field.component';
 
 interface SelectData extends FieldData {
   dataSet: Array<{name: string; value: string}>;
+  populate?: {
+    collection: string;
+    nameKey: string;
+    valueKey?: string;
+    orderBy?: string;
+    filter?: {
+      key: string;
+      operator: FirebaseOperator;
+      value: any;
+    };
+  };
 }
 
 @Component({
@@ -11,4 +32,48 @@ interface SelectData extends FieldData {
   styleUrls: ['./select.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SelectComponent extends FieldComponent<SelectData> {}
+export class SelectComponent extends FieldComponent<SelectData>
+  implements OnInit {
+  constructor(
+    @Inject(COMPONENT_DATA) public cData: SelectData,
+    private afs: AngularFirestore
+  ) {
+    super(cData);
+  }
+
+  dataSet$: Observable<Array<{name: string; value: string}>>;
+  loading$ = new BehaviorSubject(true);
+
+  ngOnInit() {
+    if (this.cData.populate) {
+      this.dataSet$ = this.afs
+        .collection(this.cData.populate.collection, (ref: any) => {
+          if (this.cData.populate.orderBy) {
+            ref = ref.orderBy(this.cData.populate.orderBy);
+          }
+
+          if (this.cData.populate.filter) {
+            ref = ref.where(
+              this.cData.populate.filter.key,
+              this.cData.populate.filter.operator,
+              this.cData.populate.filter.value
+            );
+          }
+
+          return ref;
+        })
+        .valueChanges({idField: 'id'})
+        .pipe(
+          map(docs =>
+            docs.map(doc => ({
+              value: doc[this.cData.populate.valueKey || 'id'],
+              name: doc[this.cData.populate.nameKey]
+            }))
+          ),
+          tap(() => this.loading$.next(false))
+        );
+    } else {
+      this.dataSet$ = of(this.cData.dataSet);
+    }
+  }
+}
