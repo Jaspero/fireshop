a39bf92ab3c294a531494939664982529c110bd8
Filip Lauc <filip.lauc93@gmail.com>
2019-07-02 20:58:57 +0200

    fix(DropzoneDirective): incorrect naming

--- client/src/app/shared/directives/dropzone/dropzone.directive.ts
+++ client/src/app/shared/directives/dropzone/dropzone.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
 
 @Directive({
-  selector: '[dropzone]'
+  selector: '[jmsDropzone]'
 })
 export class DropzoneDirective {
   @Output() dropped = new EventEmitter<FileList>();
