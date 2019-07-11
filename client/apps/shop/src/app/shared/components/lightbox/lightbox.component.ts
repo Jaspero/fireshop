b469dd23e8e10e5fb8897751d87baf84a373757b
Filip Lauc <filip.lauc93@gmail.com>
2019-07-11 15:06:26 +0200

    chore(LightBox): sending initial index :zap:

--- client/apps/shop/src/app/shared/components/lightbox/lightbox.component.ts
+++ client/apps/shop/src/app/shared/components/lightbox/lightbox.component.ts
@@ -13,12 +13,19 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LightboxComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {images: string[]}) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {images: string[]; initialSlide?: number}
+  ) {}
 
   sliderNeeded: boolean;
   sliderIndex = 0;
 
   ngOnInit() {
     this.sliderNeeded = this.data.images.length > 1;
+
+    if (this.data.initialSlide) {
+      this.sliderIndex = this.data.initialSlide;
+    }
   }
 }
