9360dafebc6a2b8ff74baebdb5139ea5f8d258f2
siclol <marko.milky@gmail.com>
2019-07-11 13:47:19 +0200

    feat(lightbox): style closes #189

--- client/apps/shop/src/app/pages/landing/landing.component.ts
+++ client/apps/shop/src/app/pages/landing/landing.component.ts
@@ -1,6 +1,4 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {MatDialog} from '@angular/material';
-import {LightboxComponent} from '../../shared/components/lightbox/lightbox.component';
 
 @Component({
   selector: 'jfs-landing',
@@ -9,21 +7,7 @@ import {LightboxComponent} from '../../shared/components/lightbox/lightbox.compo
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingComponent {
-  constructor(private dialog: MatDialog) {}
+  constructor() {}
 
   year = new Date().getFullYear();
-
-  openLightbox() {
-    this.dialog.open(LightboxComponent, {
-      data: {
-        images: [
-          'https://firebasestorage.googleapis.com/v0/b/jaspero-site.appspot.com/o/foodiesfeed.com_' +
-            'fresh-carrots-from-a-market.jpg?alt=media&token=ecae1723-80b1-416d-88ad-dc276ceacbb3',
-
-          'https://firebasestorage.googleapis.com/v0/b/jaspero-site.appspot.com/o' +
-            '/foodiesfeed.com_strawberry-mango-peach-smoothies.jpg?alt=media&token=cf0873e1-cf55-4ff3-8ad1-bb949591b91e'
-        ]
-      }
-    });
-  }
 }
