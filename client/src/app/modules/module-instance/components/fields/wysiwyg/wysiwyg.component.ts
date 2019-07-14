41cf4e9296f2c7fb3d36fa9b15efc16ecbde48fc
Filip Lauc <filip.lauc93@gmail.com>
2019-07-14 13:10:39 +0200

    feat(InstanceSingle): connected image upload :tada: closes #81

--- client/src/app/modules/module-instance/components/fields/wysiwyg/wysiwyg.component.ts
+++ client/src/app/modules/module-instance/components/fields/wysiwyg/wysiwyg.component.ts
@@ -8,15 +8,20 @@ import {
   TemplateRef,
   ViewChild
 } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AngularFireStorage} from '@angular/fire/storage';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatDialog} from '@angular/material';
-import {filter} from 'rxjs/operators';
+import {filter, take} from 'rxjs/operators';
+import {ModuleInstanceComponent} from '../../../module-instance.component';
 import {COMPONENT_DATA} from '../../../utils/create-component-injector';
 import {FieldComponent, FieldData} from '../../field/field.component';
 import 'tinymce/plugins/code';
 import 'tinymce/plugins/print';
 import 'tinymce/plugins/wordcount';
 import 'tinymce/plugins/link';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/fullscreen';
 
 declare const tinymce: any;
 
@@ -31,7 +36,9 @@ export class WysiwygComponent extends FieldComponent<FieldData>
   constructor(
     @Inject(COMPONENT_DATA) public cData: FieldData,
     private fb: FormBuilder,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private storage: AngularFireStorage,
+    private moduleInstance: ModuleInstanceComponent
   ) {
     super(cData);
   }
@@ -74,7 +81,16 @@ export class WysiwygComponent extends FieldComponent<FieldData>
     tinymce.init({
       target: this.textarea.nativeElement,
       height: 420,
-      plugins: ['code', 'print', 'wordcount', 'link'],
+      plugins: [
+        'code',
+        'print',
+        'wordcount',
+        'link',
+        'image',
+        'imagetools',
+        'fullscreen'
+      ],
+      image_advtab: true,
 
       /**
        * Link settings
@@ -90,11 +106,32 @@ export class WysiwygComponent extends FieldComponent<FieldData>
         'alignleft aligncenter alignright alignjustify',
         'bullist numlist outdent indent',
         'link',
-        'mediaLibrary',
-        'youTube'
+        'image',
+        'youTube',
+        'fullscreen'
       ].join(' | '),
+
+      images_upload_handler: (blobInfo, success, failure) => {
+        let module;
+
+        this.moduleInstance.module$.pipe(take(1)).subscribe(data => {
+          module = data;
+        });
+
+        this.storage
+          .upload(blobInfo.filename(), blobInfo.blob(), {
+            customMetadata: {
+              collection: module.id
+            }
+          })
+          .then(data => data.ref.getDownloadURL())
+          .then(url => success(url))
+          .catch(error => failure(error.toString()));
+      },
+
       setup: editor => {
         this.editor = editor;
+
         editor.on('keyup change', () => {
           const tinyContent = editor.getContent();
           this.cData.control.setValue(tinyContent);
@@ -102,14 +139,17 @@ export class WysiwygComponent extends FieldComponent<FieldData>
 
         editor.ui.registry.addButton('youTube', {
           type: 'button',
-          icon: 'image',
+          icon: 'embed',
           tooltip: 'Embed youtube video',
           onAction: () => {
             this.ytForm.reset(this.ytDefault);
             this.dialog
               .open(this.youTubeDialogTemplate, {width: '500px'})
               .afterClosed()
-              .pipe(filter(value => !!value))
+              .pipe(
+                filter(value => !!value),
+                take(1)
+              )
               .subscribe(() => {
                 const data = this.ytForm.getRawValue();
                 let url = data.privacyEnhancedMode
