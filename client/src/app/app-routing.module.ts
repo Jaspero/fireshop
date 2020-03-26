478841cfc8810583d310d9b94128f67b64211b1f
Filip Lauc <filip.lauc93@gmail.com>
2020-03-26 15:59:23 +0100

    chore(client): added scrollPositionRestoration

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -98,7 +98,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'top'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
