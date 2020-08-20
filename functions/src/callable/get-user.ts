5af9a5284ba78c11b1aa8e256acbcac9b464152e
Filip Lauc <filip.lauc93@gmail.com>
2020-08-20 17:50:57 +0200

    feat(functions): added roles restriction to callable functions

--- functions/src/callable/get-user.ts
+++ functions/src/callable/get-user.ts
@@ -2,7 +2,7 @@ import {auth} from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 export const getUser = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
+  if (!context.auth || !context.auth.token.role) {
     throw new functions.https.HttpsError(
       'failed-precondition',
       'The function must be called ' + 'while authenticated.'
--- functions/src/callable/remove-user.ts
+++ functions/src/callable/remove-user.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import {auth} from 'firebase-admin';
 
 export const removeUser = functions.https.onCall(async (data, context) => {
-  if (!context.auth) {
+  if (!context.auth || !context.auth.token.role) {
     throw new functions.https.HttpsError(
       'failed-precondition',
       'The function must be called ' + 'while authenticated.'
