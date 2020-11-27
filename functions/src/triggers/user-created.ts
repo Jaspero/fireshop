ee17d66ef169e6aa175be544531a3b678752b125
Filip Luac <oliveripepa1207>
2020-11-27 11:28:22 +0100

    fix(functions): added region to user triggers

--- functions/src/triggers/user-created.ts
+++ functions/src/triggers/user-created.ts
@@ -1,47 +1,52 @@
 import {auth, firestore} from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import {STATIC_CONFIG} from '../consts/static-config.const';
 import {FirestoreCollection} from '../enums/firestore-collections.enum';
 
-export const userCreated = functions.auth.user().onCreate(async user => {
-  const documentRef = await firestore()
-    .doc('settings/user')
-    .get();
-  const roles: Array<{role: string; email: string}> =
-    (documentRef.data() || {}).roles || [];
-  const role = roles.find(ro => ro.email === user.email);
+export const userCreated = functions
+  .region(STATIC_CONFIG.cloudRegion)
+  .auth
+  .user()
+  .onCreate(async user => {
+    const documentRef = await firestore()
+      .doc('settings/user')
+      .get();
+    const roles: Array<{role: string; email: string}> =
+      (documentRef.data() || {}).roles || [];
+    const role = roles.find(ro => ro.email === user.email);
 
-  if (role) {
-    const customClaims = {
-      role: role.role
-    };
-
-    auth().setCustomUserClaims(user.uid, customClaims)
-      .catch(error => {
-        console.error('Setting custom claims', error);
-      });
-  }
-
-  firestore()
-    .collection(FirestoreCollection.Users)
-    .doc(user.uid)
-    .set({
-      createdOn: Date.now(),
-      email: user.email,
-
-      /**
-       * Assign providerData if it's an admin
-       * for easier reference
-       */
-      ...role ? {
-        providerData: user.providerData.map((it: any) => it.providerId),
+    if (role) {
+      const customClaims = {
         role: role.role
-      } : {
-        role: ''
-      }
-    })
-    .catch(error => {
-      console.error('Creating user', error);
-    });
+      };
 
-  return true;
-});
+      auth().setCustomUserClaims(user.uid, customClaims)
+        .catch(error => {
+          console.error('Setting custom claims', error);
+        });
+    }
+
+    firestore()
+      .collection(FirestoreCollection.Users)
+      .doc(user.uid)
+      .set({
+        createdOn: Date.now(),
+        email: user.email,
+
+        /**
+         * Assign providerData if it's an admin
+         * for easier reference
+         */
+        ...role ? {
+          providerData: user.providerData.map((it: any) => it.providerId),
+          role: role.role
+        } : {
+          role: ''
+        }
+      })
+      .catch(error => {
+        console.error('Creating user', error);
+      });
+
+    return true;
+  });
--- functions/src/triggers/user-deleted.ts
+++ functions/src/triggers/user-deleted.ts
@@ -1,12 +1,17 @@
 import {firestore} from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import {STATIC_CONFIG} from '../consts/static-config.const';
 import {FirestoreCollection} from '../enums/firestore-collections.enum';
 
-export const userDeleted = functions.auth.user().onDelete(async user => {
-  try {
-    await firestore()
-      .collection(FirestoreCollection.Users)
-      .doc(user.uid)
-      .delete();
-  } catch (e) {}
-});
+export const userDeleted = functions
+  .region(STATIC_CONFIG.cloudRegion)
+  .auth
+  .user()
+  .onDelete(async user => {
+    try {
+      await firestore()
+        .collection(FirestoreCollection.Users)
+        .doc(user.uid)
+        .delete();
+    } catch (e) {}
+  });
